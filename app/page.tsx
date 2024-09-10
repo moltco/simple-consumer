bdbac04365953ec75155560296df1f07c265fa14
moltco <67069242+moltco@users.noreply.github.com>
2024-09-10 16:20:56 +0100

    import error reproduced

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import { ExportedClass, ExportedEnum, ExportedType } from "simple-package";
 
 export default function Home() {
+  const msg: ExportedType = {
+    message: "I am warning you",
+    severity: ExportedEnum.warning,
+  };
+
+  const importedClass = new ExportedClass(msg);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -22,7 +30,10 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <div className="flex gap-4 items-center flex-col sm:flex-row text-lg">
+          <p>Here is what we imported:</p>
+          {importedClass.toExportString()}
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
